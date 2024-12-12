ef44f3ef499eb846f1db63457090e9ac3bcc8d6c
Akshay-C-A <akshayanil3103@gmail.com>
2024-12-12 09:58:35 +0530

    tutorial one

--- src/App.js
+++ src/App.js
@@ -2,9 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Akshay";
+  const name2 = <h1>C A</h1>;
   return (
     <div className="App">
-      Hello World
+      <h1>Hello {name}</h1>
+      {name2}
+
+    < User />
+    < User2 name="Akshay Anil" age={22} />
+
+
+    </div>
+
+  );
+}
+
+const User = () => {
+  return(
+    <div>
+      <h3>Akshay C A</h3>
+      <h3>Age : 21</h3>
+    </div>
+  );
+}
+
+const User2 = (props) => {
+  return(
+    <div>
+      <h3>{props.name}</h3>
+      <h3>{props.age}</h3>
     </div>
   );
 }
