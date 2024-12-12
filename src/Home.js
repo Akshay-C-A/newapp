b013ca00cbafb081261858c828d5eb6e24749a0b
Akshay-C-A <akshayanil3103@gmail.com>
2024-12-12 12:46:05 +0530

    #8 Using setState

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,30 @@
+import { useState } from "react";
+
 const Home = () => {
-    const handleClick = () =>{
-        alert("Button Clicked");
+    // const handleClick = () =>{
+    //     alert("Button Clicked");
+    // }
+    // const handleClickAgain = (name) => {
+    //     alert("Button Clicked By "+name);
+    // }
+
+    const [name,setName] = useState("Akshay Anil");
+    const [age, setAge] = useState(20);
+    const handleName = () =>{
+        setName("Akshay C A");
+        setAge(21);
     }
-    const handleClickAgain = (name) => {
-        alert("Button Clicked By "+name);
-    }
+
     return ( 
         <div className="home">
             <h3>Home Page</h3>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={() => {handleClickAgain("Akshay")}}>Click Me Again</button> 
+            <p>{name} is {age} years old</p>
+
+            {/* <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {handleClickAgain("Akshay")}}>Click Me Again</button>  */}
             {/* can use 'e' in anonyamus function to get event logs and details pass e as a parameter in handleClickAgain function inside*/}
+
+            <button onClick={handleName}>Change Name</button>
         </div>
     );
 }
