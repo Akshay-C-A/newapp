72af2778cdc7f95a60828902f425ced3faef309d
Akshay-C-A <akshayanil3103@gmail.com>
2024-12-12 12:20:29 +0530

    #7 click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    const handleClick = () =>{
+        alert("Button Clicked");
+    }
+    const handleClickAgain = (name) => {
+        alert("Button Clicked By "+name);
+    }
     return ( 
-        <h3>HOME PAGE</h3>
-     );
+        <div className="home">
+            <h3>Home Page</h3>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {handleClickAgain("Akshay")}}>Click Me Again</button> 
+            {/* can use 'e' in anonyamus function to get event logs and details pass e as a parameter in handleClickAgain function inside*/}
+        </div>
+    );
 }
- 
+
 export default Home;
