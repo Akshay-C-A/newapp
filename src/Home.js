9a91cedb47b1725db896a7e4474ede31f03637c9
Akshay-C-A <akshayanil3103@gmail.com>
2024-12-15 05:05:33 +0530

    Making a custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 // const Home = () => {
 //     // const handleClick = () =>{
@@ -38,11 +39,6 @@ const Home = () => {
     //     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     //   ]);
 
-    const [blogs,setBlogs] = useState(null);
-    const [isPending,setIsPending] = useState(true);
-    const [error,setError] = useState(null);
-
-
     // const handleDelete = (id) => {
     //     const newBlogs = blogs.filter( (blog) => blog.id !== id);
     //     setBlogs(newBlogs);
@@ -54,24 +50,7 @@ const Home = () => {
     //     console.log("Used useEffects");
     // },[name])
 
-    useEffect( () => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            if(!res.ok){
-                throw Error("Could not fetch data for that resource");
-            }
-            return res.json();
-        })
-        .then((data) => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null)
-        })
-        .catch(err => {
-            setIsPending(false)
-            setError(err.message)
-        })
-    },[])
+    const {data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs")
 
     return ( 
         <div className="home">
